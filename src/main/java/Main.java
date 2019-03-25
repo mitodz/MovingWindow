f1c5400387f20cf4778d0750b8ddc61447674043
mitodz <mitodzzz@gmail.com>
2019-03-25 21:16:04 +0300

    correct version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,57 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Main {
 
-        void run() throws FileNotFoundException {
-
+    void run() throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("input.txt"));
         int n = scanner.nextInt();
         int[] a = new int[n]; //первоначальный массив
-        ArrayDeque<Integer> q = new ArrayDeque<>();
 
         for (int k = 0; k < n; k++) {
             a[k] = scanner.nextInt();
         }
 
         int m1 = a[0];//максимальный элемент
-        int index = 0;//максимальный элемент
+        int i1 = 0;//индекс 1 максимального элемента
         int m2 = -1;//максимальный после первого
+        int i2 = 0;//индекс 2 максимального элемента
         int w = scanner.nextInt(); //размер окна
-
-        for (int i = 0; i < w; i++) { //первоначальное заполнение очереди элементами из окна
-            q.add(a[i]);
+        int[] q = new int[n]; //массив, симулирующий далее очередь
+        for (int i = 0; i < w; i++) { //первоначальное заполнение очереди (массива) элементами из окна
+            q[i] = a[i];
         }
 
         for (int j = 1; j < w; j++) { //максимум в первой части окна
-            if (a[j] > m1) {
+            if (a[j] >= m1) {
                 m1 = a[j];
-                index = j;
+                i1 = j;
             }
         }
 
-        for (int i = index + 1; i < w; i++) { //максимум во второй части окна
+        for (int i = i1 + 1; i < w; i++) { //максимум во второй части окна
             if (a[i] > m2) {
                 m2 = a[i];
+                i2 = i;
             }
         }
 
         System.out.print(m1 + " "); //выводим первый максимальный элемент в окне
         for (int i = w; i < n; i++) { //выводим оставшиеся
-            if (q.poll() == m1) {
+            if (i - w == i1) { //в случае, если "вылетающий из окна элемент по индексу равен текущему максимальному
+                //то меняем его на второй максимум, заодно и индекс.
                 m1 = m2;
+                i1 = i2;
                 m2 = -1;
             }
-            q.add(a[i]);
+            q[i] = a[i];
             if (m1 < a[i]) {
                 m1 = a[i];
+                i1 = i;
             } else if (m2 < a[i]) {
                 m2 = a[i];
+                i2 = i;
             }
             System.out.print(m1 + " ");
         }
@@ -56,6 +59,6 @@ public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-    new Main().run();
+        new Main().run();
     }
 }
