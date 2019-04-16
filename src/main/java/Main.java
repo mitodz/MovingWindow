6cf4c86b6e45d83148a241b082daa9308a74b9b5
mitodz <mitodzzz@gmail.com>
2019-04-16 19:07:21 +0300

    Probably works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,52 @@
+import javafx.util.Pair;
+
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
     public String windowMax(int n, int[] a, int w) {
-        int m1 = a[0];//максимальный элемент
-        int i1 = 0;//индекс 1 максимального элемента
-        int m2 = -1;//максимальный после первого
-        int i2 = 0;//индекс 2 максимального элемента
+        int m1 = a[0];
         StringBuilder sb = new StringBuilder();
+        Stack<Pair<Integer, Integer>> s1 = new Stack<>();
+        Stack<Pair<Integer, Integer>> s2 = new Stack<>();
+        for (int i = 0; i < w; i++) {
+            Integer max = s1.empty() ? a[i] : Integer.max (a[i], s1.peek().getValue());
+            s1.push (new Pair<> (a[i], max));
+        }
+        if (s1.empty() || s2.empty())
+            m1 = s1.isEmpty() ? s2.peek().getValue() : s1.peek().getValue();
+        else
+            m1 = Integer.min (s1.peek().getValue(), s2.peek().getValue());
 
-        for (int j = 1; j < w; j++) { //максимум в первой части окна
-            if (a[j] >= m1) {
-                m1 = a[j];
-                i1 = j;
-            }
+        sb.append(m1 + " ");
+
+        for (int i = w; i < n; i++) {
+            Integer max = s1.empty() ? a[i] : Integer.max (a[i], s1.peek().getValue());
+            s1.push (new Pair<> (a[i], max));
+
+            if (s2.empty())
+                while (!s1.empty()) {
+                    int element = s1.peek().getKey();
+                    s1.pop();
+                    m1 = s2.empty() ? element : Integer.max (element, s2.peek().getValue());
+                    s2.push (new Pair<>  (element, m1));
+                }
+            s2.pop();
+
+            if (s1.empty() || s2.empty())
+                m1 = s1.isEmpty() ? s2.peek().getValue() : s1.peek().getValue();
+            else
+                m1 = Integer.max (s1.peek().getValue(), s2.peek().getValue());
+
+            sb.append(m1 + " ");
         }
 
-        for (int i = i1 + 1; i < w; i++) { //максимум во второй части окна
-            if (a[i] > m2) {
-                m2 = a[i];
-                i2 = i;
-            }
-        }
-
-        sb.append(m1).append(" "); //выводим первый максимальный элемент в окне
-        for (int i = w; i < n; i++) { //выводим оставшиеся
-            if (i - w == i1) { //в случае, если "вылетающий" из окна элемент по индексу равен текущему максимальному
-                //то меняем его на второй максимум, заодно и индекс.
-                m1 = m2;
-                i1 = i2;
-                m2 = -1;
-            }
-            if (m1 <= a[i]) {
-                m1 = a[i];
-                i1 = i;
-                i2=i;
-                sb.append(m1).append(" ");
-                m2=-1;
-                continue;
-            }
-            if (m2 <= a[i]) {
-                m2 = a[i];
-                i2 = i;
-                if (m1==m2) i1=i2;
-            }
-            sb.append(m1).append(" ");
-        }
         return sb.toString();
     }
 
