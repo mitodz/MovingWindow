b5cc7f309e43258032316902e72a0962d8cbab3f
mitodz <mitodzzz@gmail.com>
2019-04-16 19:13:44 +0300

    Probably works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import javafx.util.Pair;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,11 +7,11 @@ public class Main {
     public String windowMax(int n, int[] a, int w) {
         int m1;
         StringBuilder sb = new StringBuilder();
-        Stack<Pair<Integer, Integer>> s1 = new Stack<>();
-        Stack<Pair<Integer, Integer>> s2 = new Stack<>();
+        Stack<HashMap.SimpleEntry<Integer, Integer>> s1 = new Stack<>();
+        Stack<HashMap.SimpleEntry<Integer, Integer>> s2 = new Stack<>();
         for (int i = 0; i < w; i++) {
             Integer max = s1.empty() ? a[i] : Integer.max (a[i], s1.peek().getValue());
-            s1.push (new Pair<> (a[i], max));
+            s1.push (new HashMap.SimpleEntry<> (a[i], max));
         }
         if (s1.empty() || s2.empty())
             m1 = s1.isEmpty() ? s2.peek().getValue() : s1.peek().getValue();
@@ -22,14 +22,14 @@ public class Main {
 
         for (int i = w; i < n; i++) {
             Integer max = s1.empty() ? a[i] : Integer.max (a[i], s1.peek().getValue());
-            s1.push (new Pair<> (a[i], max));
+            s1.push (new HashMap.SimpleEntry<> (a[i], max));
 
             if (s2.empty())
                 while (!s1.empty()) {
                     int element = s1.peek().getKey();
                     s1.pop();
                     m1 = s2.empty() ? element : Integer.max (element, s2.peek().getValue());
-                    s2.push (new Pair<>  (element, m1));
+                    s2.push (new HashMap.SimpleEntry<>  (element, m1));
                 }
             s2.pop();
 
